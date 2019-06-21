6cbf66f80f8cd0928a6f1dafbfcf3731d41a2b8e
Eric Storey <estorey@gmail.com>
2019-06-21 19:20:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,24 @@ class Driver{
  
         // insert in the user to the store
         //store.users.push(this);
+        return this
     }
 }
 
 let passengerId=0
 class Passenger{
+   constructor(name) {
+        this.id = ++passengerId;
+        this.name = name;
+ 
+        // insert in the user to the store
+        //store.users.push(this);
+        return this
+    }
+}
+
+let tripId=0
+class Driver{
    constructor(name) {
         this.id = ++driverId;
         this.name = name;
