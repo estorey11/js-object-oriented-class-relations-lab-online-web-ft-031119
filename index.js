99d7495c42e9468e243940bd698cf02ddef40382
Eric Storey <estorey@gmail.com>
2019-06-21 19:11:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 let driverId=0
 class Driver{
-  
+   constructor(name) {
+        this.id = ++userId;
+        this.name = name;
+ 
+        // insert in the user to the store
+        store.users.push(this);
+    }
 }
 
 
