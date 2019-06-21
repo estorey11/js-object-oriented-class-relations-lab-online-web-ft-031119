efd9a8bd5c86f16fc88ea82f8e13d2ecf61aa1f1
Eric Storey <estorey@gmail.com>
2019-06-21 19:36:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,6 +53,22 @@ class Trip{
         return this
         
     }
+    
+    driver() {
+        return store.drivers.find(
+            function(driver) {
+                return driver.id === this.driverId;
+            }.bind(this)
+        );
+    }
+    
+    passenger() {
+        return store.passengers.find(
+            function(passenger) {
+                return passenger.id === this.passengerId;
+            }.bind(this)
+        );
+    }
 }
 
 
